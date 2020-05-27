7412b19d077e0e9b198e59283f2b2993614561f8
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-27 16:25:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,10 @@
 def unsafe?(speed)
-  if speed <40 || speed >60
+  if speed <40 || 60 < speed
     return true
   else
     return false
   end 
 end
-
 
 def not_safe?(speed)
 	speed >60 || speed <40? true:false
